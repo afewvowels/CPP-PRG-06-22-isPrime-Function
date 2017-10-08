d1725b0a21618484cb61ab40d4df3c77fd4c85d7
Keith Smith <aeiouandsometimesy@me.com>
2017-10-08 09:44:53 -0400

    Added special case for 1 and comments explaining code.

--- PRG-6-22-isPrime-Function/main.cpp
+++ PRG-6-22-isPrime-Function/main.cpp
@@ -2,14 +2,69 @@
 //  main.cpp
 //  PRG-6-22-isPrime-Function
 //
-//  Created by Keith Smith on 10/8/17.
+//  Created by Keith Smith on 9/21/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  A prime number is a number that is only divisible by itself and 1. For example,
+//  the number 5 is prime because it can only be evenly divided by 1 and 5. The number 6,
+//  however, is not prime because it can be divided evenly by 1, 2, 3, and 6.
+//
+//  Write a function name isPrime, which takes an integer as an argument and returns
+//  true if the argument is a prime number, or false otherwise. Demonstrate the fucntion
+//  in a complete program.
+//
+//  TIP: Recall that the % operator divides one number by another, and returns the remainder
+//  of the division. In an expression such as num1 % num2, the % operator will return 0
+//  if num1 is evenly divisible by num2.
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+bool testPrime(int);
+
+int main() {
+    
+    bool isPrime;
+    
+    int intNumberToTest;
+    
+    cout << "1 is prime\n"; // Special case for first number not accounted for by loop
+    
+    // Loop to test integers 1 through 100,
+    // will return true/false isPrime for this range
+    for(int i = 0 ; i <= 100 ; i++)
+    {
+        intNumberToTest = i;
+        
+        isPrime = testPrime(intNumberToTest);
+    }
+    
     return 0;
 }
+
+bool testPrime(int intNumberToTest)
+{
+    int intRemainder;
+    
+    for(int j = 1 ; j <= intNumberToTest ; j++)
+    {
+        intRemainder = intNumberToTest % j;
+        
+        // Output result to console
+        if(intRemainder == 0 && j != intNumberToTest && j != 1) // Need to exclude divisible by one
+                                                                // means we can't test 1 here, hence
+                                                                // special case earlier
+        {
+            cout << intNumberToTest << " is not prime" << endl;
+            break;
+        }
+        else if(intRemainder == 0 && j == intNumberToTest && j != 1)
+        {
+            cout << intNumberToTest << " is prime" << endl;
+        }
+    }
+    
+    return false;
+}
+
